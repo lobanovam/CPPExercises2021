18ffd1406dc3237008e4ca6b3557c2bd52606808
Александр <alexmlobanov@gmail.com>
2021-10-12 20:36:12 +0300

    disjoint

--- lesson04/src/disjoint_set.cpp
+++ lesson04/src/disjoint_set.cpp
@@ -3,8 +3,7 @@
 #define ROOT -1 // объявили макрос (считайте константу) равный минус единице - чтобы не было "волшебной переменной", а была именованная константа "корень дерева"
 
 
-DisjointSet::DisjointSet(int size)
-{
+DisjointSet::DisjointSet(int size) {
     parents = std::vector<int>(size);
     ranks = std::vector<int>(size);
     sizes = std::vector<int>(size);
@@ -13,6 +12,15 @@ DisjointSet::DisjointSet(int size)
     // TODO заполнить parents
     // TODO заполнить ranks
     // TODO заполнить sizes
+    for (int i = 0; i < size; i++) {
+        parents[i] = -1;
+    }
+    for (int i = 0; i < size; i++) {
+        ranks[i] = 0;
+    }
+    for (int i = 0; i<size; i++) {
+        sizes[i] = 1;
+    }
 }
 
 int	DisjointSet::get_set(int element)
@@ -20,7 +28,12 @@ int	DisjointSet::get_set(int element)
     // TODO по номеру элемента нужно переходя по ссылкам на родителя дойти до самого верхнего элемента,
     // номер этого корневого элемента - номер множества на данный момент (кто вверху тот и главный, множество названо в его честь)
     int first_of_their_kind = 0;
-
+    int i = element;
+    if (parents[i] != i && parents[i]!= -1) {
+        parents[i] = get_set(parents[element]);
+    }else {
+        first_of_their_kind = i;
+    }
     return first_of_their_kind;
 }
 
@@ -29,15 +42,20 @@ int DisjointSet::count_differents() const
     // TODO посчитать сколько разных множеств (подсказка: в каждом множестве ровно один корень, а корень - это тот у кого родитель = ROOT)
     int count = 0;
     for (size_t i = 0; i < this->parents.size(); i++) {
-        // ...
+        if (parents[i]==-1) count++;
     }
     return count;
 }
 
 int DisjointSet::get_set_size(int element)
 {
+    int c = 0;
+    int root = get_set(element);
+    for (int i = 0; i < this->parents.size(); i++) {
+        if (get_set(i)==root) c++;
+    }
     // TODO сообщить сколько элементов в множестве, которому принадлежит данный элемент (да, это очень просто)
-    return 0;
+    return c;
 }
 
 int	DisjointSet::union_sets(int element0, int element1)
@@ -46,6 +64,22 @@ int	DisjointSet::union_sets(int element0, int element1)
     // кого из них подвесить к другому (тем самым объединить два множества)
     // при этом стоит подвешивать менее высокое дерево к более высокому (т.е. учитывая ранк),
     // а так же важно не забыть после переподвешивания у корневого элемента обновить ранк и размер множества
+    element0 = get_set(element0);
+    element1 = get_set(element1);
+    int set_position = 0;
+    if (element1 == element0) {
+        return element0;
+    }
+    if (ranks[element0]==ranks[element1]) {
+        parents[element0] = element1;
+        set_position = element1;
+    }
+    if (ranks[element0]<ranks[element1]) {
+        ranks[element0]++;
+    }else{
+        parents[element1] = element0;
+        set_position = element0;
+    }
 
-    return 0; // вернуть номер объединенного множества
+    return set_position; // вернуть номер объединенного множества
 }
