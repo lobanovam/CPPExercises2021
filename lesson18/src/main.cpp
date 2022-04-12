ce6ad64dc23868f27e2983160f095c6f15ee7b16
Александр <alexmlobanov@gmail.com>
2022-04-12 23:09:49 +0300

    done shit

--- lesson18/src/main.cpp
+++ lesson18/src/main.cpp
@@ -14,18 +14,15 @@
 #include <libutils/fast_random.h>
 
 
-
 int estimateQuality(cv::Mat mat, int j, int i, int ny, int nx);
 
 // Эта функция говорит нам правда ли пиксель отмаскирован, т.е. отмечен как "удаленный", т.е. белый
 bool isPixelMasked(cv::Mat mask, int j, int i) {
-    rassert(j >= 0 && j < mask.rows, 372489347280017);
-    rassert(i >= 0 && i < mask.cols, 372489347280018);
-    rassert(mask.type() == CV_8UC3, 2348732984792380019);
+
     if (mask.at<cv::Vec3b>(j, i) == cv::Vec3b(255, 255, 255)) {
         return true;
     }
-    // TODO проверьте белый ли пиксель
+        // TODO проверьте белый ли пиксель
     else return false;
 }
 
@@ -77,12 +74,12 @@ void run(int caseNumber, std::string caseName) {
     cv::Mat image = original;
     int k = 100;
     while (k > 0) {
-        for (int j = 2; j < image.rows-3; j++) {
-            for (int i =2; i < image.cols-3; i++) {
-                if (!isPixelMasked(mask,j,i)) {
+        for (int j = 0; j < image.rows-3; j++) {
+            for (int i = 0; i < image.cols - 3; i++) {
+                if (!isPixelMasked(mask, j, i)) {
                     continue;
                 }
-                cv::Vec2i dxy = shifts.at<cv::Vec2i>(j,i);
+                cv::Vec2i dxy = shifts.at<cv::Vec2i>(j, i);
                 int nx = i + dxy[1];
                 int ny = j + dxy[0];
                 int currentQuality = estimateQuality(image, j, i, ny, nx);
@@ -90,52 +87,37 @@ void run(int caseNumber, std::string caseName) {
                 int rand_x = 0;
                 int rand_y = 0;
                 while (b) {
-                    rand_x = random.next(2, image.cols -3);
-                    rand_y = random.next(2, image.rows -3);
+                    rand_x = random.next(2, image.cols - 3);
+                    rand_y = random.next(2, image.rows - 3);
                     if (!isPixelMasked(image, rand_y, rand_x)) {
                         b = false;
                     }
                 }
                 int randomQuality = estimateQuality(image, j, i, rand_y, rand_x);
                 if (randomQuality < currentQuality || currentQuality == 0) {
-                    shifts.at<cv::Vec2i>(j,i)[0] = rand_y - j;
-                    shifts.at<cv::Vec2i>(j,i)[1] = rand_x - i;
-                    image.at<cv::Vec3b>(j,i) = image.at<cv::Vec3b>(rand_y,rand_x);
+                    shifts.at<cv::Vec2i>(j, i)[0] = rand_y - j;
+                    shifts.at<cv::Vec2i>(j, i)[1] = rand_x - i;
+                    image.at<cv::Vec3b>(j, i) = image.at<cv::Vec3b>(rand_y, rand_x);
                 }
+                int nqualleft = estimateQuality(image, j, i, j + shifts.at<cv::Vec2i>(j, i - 1)[0], i + shifts.at<cv::Vec2i>(j, i - 1)[1]);
 
-                int nqualleft = estimateQuality(image, j, i-1, ny, nx-1);
-                int nqualup = estimateQuality(image, j-1, i, ny-1, nx);
-                int numbers[4] {currentQuality, randomQuality, nqualleft, nqualup};
-                std::sort(numbers, numbers+4);
-                int bqual = numbers[0];
-                switch (bqual)
-                {
-                    case randomQuality:
-                        shifts.at<cv::Vec2i>(j, i)[0] = rand_y - j;
-                        shifts.at<cv::Vec2i>(j,i)[1] = rand_x - i;
-                        image.at<cv::Vec3b>(j,i) = image.at<cv::Vec3b>(rand_y,rand_x);
-                        break;
-                    case nqualleft:
-                        shifts.at<cv::Vec2i>(j, i) = shifts.at<cv::Vec2i>(j, i-1) ;
-                        image.at<cv::Vec3b>(j,i) = image.at<cv::Vec3b>(j + shifts.at<cv::Vec2b>(j,i)[0], i + shifts.at<cv::Vec2b>(j,i)[1]);
-                        break;
+                int nqualup = estimateQuality(image, j, i, j + shifts.at<cv::Vec2i>(j - 1, i)[0], i + shifts.at<cv::Vec2i>(j - 1, i)[1]);
 
-
-
-                    case nqualup:
-                        shifts.at<cv::Vec2i>(j, i) = shifts.at<cv::Vec2i>(j-1, i) ;
-                        image.at<cv::Vec3b>(j,i) = image.at<cv::Vec3b>(j + shifts.at<cv::Vec2b>(j,i)[0], i + shifts.at<cv::Vec2b>(j,i)[1]);
-                        break;
-
-
-                    case currentQuality:
-                        break;
-
+                if (randomQuality < currentQuality || currentQuality == 0) {
+                    shifts.at<cv::Vec2i>(j, i)[0] = rand_y - j;
+                    shifts.at<cv::Vec2i>(j, i)[1] = rand_x - i;
+                    image.at<cv::Vec3b>(j, i) = image.at<cv::Vec3b>(rand_y, rand_x);
                 }
-
-
-
-
+                if (nqualleft < currentQuality) {
+                    currentQuality = nqualleft;
+                    shifts.at<cv::Vec2i>(j, i) = shifts.at<cv::Vec2i>(j, i - 1);
+                    image.at<cv::Vec3b>(j, i) = image.at<cv::Vec3b>(j + shifts.at<cv::Vec2i>(j, i)[0], i + shifts.at<cv::Vec2i>(j, i)[1]);
+                }
+                if (nqualup < currentQuality) {
+                    currentQuality = nqualup;
+                    shifts.at<cv::Vec2i>(j, i) = shifts.at<cv::Vec2i>(j - 1, i);
+                    image.at<cv::Vec3b>(j, i) = image.at<cv::Vec3b>(j + shifts.at<cv::Vec2i>(j, i)[0], i + shifts.at<cv::Vec2i>(j, i)[1]);
+                }
             }
         }
 
@@ -146,12 +128,14 @@ void run(int caseNumber, std::string caseName) {
 
 }
 
+
 int estimateQuality(cv::Mat image, int j, int i, int ny, int nx) {
     int sum0 = 0;
     int sum1 = 0;
     int sum2 = 0;
-    for (int k = -2; k < 3; ++k) {
-        for (int l = -2; l < 3; ++l) {
+
+    for (int k = -2; k <3; ++k) {
+        for (int l = -2; l <3; ++l) {
             sum0 += abs(image.at<cv::Vec3b>(j+k, i+l)[0] - image.at<cv::Vec3b>(ny+k, nx+l)[0]);
             sum1 += abs(image.at<cv::Vec3b>(j+k, i+l)[1] - image.at<cv::Vec3b>(ny+k, nx+l)[1]);
             sum2 += abs(image.at<cv::Vec3b>(j+k, i+l)[2] - image.at<cv::Vec3b>(ny+k, nx+l)[2]);
@@ -159,44 +143,6 @@ int estimateQuality(cv::Mat image, int j, int i, int ny, int nx) {
     }
     return (sum0 + sum1 + sum2);
 }
-
-
-// TODO 10 создайте картинку хранящую относительные смещения - откуда брать донора для заплатки, см. подсказки про то как с нею работать на сайте
-// TODO 11 во всех отмаскированных пикселях: заполните эту картинку с относительными смещениями - случайными смещениями (но чтобы они и их окрестность 5х5 не выходила за пределы картинки)
-// TODO 12 во всех отмаскированных пикселях: замените цвет пиксела А на цвет пикселя Б на который указывает относительное смещение пикселя А
-// TODO 13 сохраните получившуюся картинку на диск
-// TODO 14 выполняйте эти шаги 11-13 много раз, например 1000 раз (оберните просто в цикл, сохраняйте картинку на диск только на каждой десятой или сотой итерации)
-// TODO 15 теперь давайте заменять значение относительного смещения на новой только если новая случайная гипотеза - лучше старой, добавьте оценку "насколько смещенный патч 5х5 похож на патч вокруг пикселя если их наложить"
-//
-// Ориентировочный псевдокод-подсказка получившегося алгоритма:
-// cv::Mat shifts(...); // матрица хранящая смещения, изначально заполнена парами нулей
-// cv::Mat image = original; // текущая картинка
-// for (100 раз) {
-//     for (пробегаем по всем пикселям j,i) {
-//         if (если этот пиксель не отмаскирован)
-//             continue; // пропускаем т.к. его менять не надо
-//         cv::Vec2i dxy = смотрим какое сейчас смещение для этого пикселя в матрице смещения
-//         int (nx, ny) = (i + dxy.x, j + dxy.y); // ЭТО НЕ КОРРЕКТНЫЙ КОД, но он иллюстрирует как рассчитать координаты пикселя-донора из которого мы хотим брать цвет
-//         currentQuality = estimateQuality(image, j, i, ny, nx, 5, 5); // эта функция (создайте ее) считает насколько похож квадрат 5х5 приложенный центром к (i, j)
-//                                                                                                                        на квадрат 5х5 приложенный центром к (nx, ny)
-//
-//         int (rx, ry) = random.... // создаем случайное смещение относительно нашего пикселя, воспользуйтесь функцией random.next(...);
-//                                      (окрестность вокруг пикселя на который укажет смещение - не должна выходить за пределы картинки и не должна быть отмаскирована)
-//         randomQuality = estimateQuality(image, j, i, j+ry, i+rx, 5, 5); // оцениваем насколько похоже будет если мы приложим эту случайную гипотезу которую только что выбрали
-//
-//         if (если новое качество случайной угадайки оказалось лучше старого) {
-//             то сохраняем (rx,ry) в картинку смещений
-//             и в текущем пикселе кладем цвет из пикселя на которого только что смотрели (цент окрестности по смещению)
-//             (т.е. мы не весь патч сюда кладем, а только его центральный пиксель)
-//         } else {
-//             а что делать если новая случайная гипотеза хуже чем то что у нас уже есть?
-//         }
-//     }
-//     не забываем сохранить на диск текущую картинку
-//     а как численно оценить насколько уже хорошую картинку мы смогли построить? выведите в консоль это число
-// }
-
-
 
 int main() {
     try {
